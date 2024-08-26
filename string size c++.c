0b47c2217a13b4f40a3c86853c07a5eebff2b072
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:56:41 +0530

    Create string size c++.c

---
+++ string size c++.c
@@ -0,0 +1,13 @@
+// there is no difference between the string (length and the size)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string text = "qwertyuioplkjhgfdsazxcvbnm";
+    cout << "the length of the size is :"<<text.size();
+    return 0;
+
+}
